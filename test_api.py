59e5b5f30c2417a9ebb6b5a8d745ee9fb4f5c295
marylyn554 <marylynrabe@gmail.com>
2025-12-18 01:18:11 +0100

    correction tests

--- test_api.py
+++ test_api.py
@@ -1,13 +1,18 @@
+import numpy as np
 from api import make_json_safe
 
 def test_make_json_safe_for_int():
-    resultat = make_json_safe("15")
-    assert result == 15
+    resultat = make_json_safe('15')
+    assert resultat == '15'
 
 def test_make_json_safe_for_float():
-    resultat = make_json_safe("1.5")
-    assert result == 1.5
+    resultat = make_json_safe('1.5')
+    assert resultat == '1.5'
 
 def test_make_json_safe_for_Na():
     resultat = make_json_safe(np.nan)
-    assert result == None
+    assert resultat == None
+
+def test_make_json_safe_for_Inf():
+    resultat = make_json_safe(np.inf)
+    assert resultat == None
